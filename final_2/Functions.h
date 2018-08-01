e65946503500e5d6f4033ea59f06dfbab267bb95
Gaston Pesoa <gastonpesoa@gmail.com>
2018-08-01 19:09:58 -0300

    Delete Functions.h

--- final_2/Functions.h
+++
@@ -1,6 +0,0 @@
-
-int imprimirArchivoPorLocalidad(ArrayList* entregasList, char* localidad, FILE *pFile);
-
-
-
-int generarArchivoDeReparto(FILE *pFile, char *fileName, ArrayList* entregasList, ArrayList* localidadesList);
